e9d93c4ed877616716e6b1950acb61225fea5e73
Lalita <lalilita888@gmail.com>
2022-06-22 12:04:41 -0400

    add changes in CheckoutPage and ShoppingCartPage

--- src/test/java/techTriangle_testNG/pageObjects/CheckoutPage.java
+++ src/test/java/techTriangle_testNG/pageObjects/CheckoutPage.java
@@ -2,7 +2,6 @@ package techTriangle_testNG.pageObjects;
 
 import java.util.ArrayList;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
