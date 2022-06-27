af4e383b884e2745fae925afd736f12f5c7fcffb
Lalita <lalilita888@gmail.com>
2022-06-27 01:40:33 -0400

    fix conflict

--- src/test/java/techTriangle_testNG/testing/EbikesPageTest.java
+++ src/test/java/techTriangle_testNG/testing/EbikesPageTest.java
@@ -1,43 +1,29 @@
+
 package techTriangle_testNG.testing;
 
-
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import techTriangle_testNG.utilities.CommonMethods;
 
-
-public class EbikesPageTest extends CommonMethods{
-
-
+public class EbikesPageTest extends CommonMethods {
+	
+	WebDriver driver;
 	@BeforeClass
 	public void setup() {
 		getDriver();
 	}
 
-	@Test 
-	public void EB_01_VerifyEBikesTab() {
-		ebikePage.verifyEBikesTabIsEnableAndNavigateToEBikesPage();
-	}
-	
-	@Test 
-	public void EB_02_VerifySortByPartNumber() throws InterruptedException {
-		ebikePage.verifySortByPartNumberAsc();
-	}
-	
-	@Test 
-	public void EB_03_VerifySortByDescription() throws InterruptedException {
-		ebikePage.verifySortByShortDescriptionAsc();
-	}
-
-	@Test 
-	public void EB_04_VerifySortByPrice() throws InterruptedException {
-		ebikePage.verifySortByPriceAsc();
-	}
-	
 	@AfterClass
 	public void teardown() {
-		tearDown();
+		System.out.println("browser closed");
+		getDriver().close();
 	}
-
+  
+	@Test(enabled = true)
+	public void VerifyProductListInsideEbikesPage() {
+		ebikePage.verifyBikeImageCount();
+	}
+	
 }
